174a42c780535323df9f0909453e9506fc4b3205
Dan <daniel.speranskiy@live.com>
2023-12-12 19:42:29 +0400

    Added?

--- src/components/inputs/textInput/components/TextInput.tsx
+++ src/components/inputs/textInput/components/TextInput.tsx
@@ -51,9 +51,15 @@ export const TextInput = (props: Props) => {
       style={{
         'border-top': '1px solid #eeeeee',
         position: 'absolute',
+<<<<<<< HEAD
         left: '20px',
         right: '20px',
         bottom: '40px',
+=======
+        left: '2px',
+        right: '2px',
+        bottom: '20px',
+>>>>>>> 0102b37 (Added changes)
         margin: 'auto',
         'z-index': 1000,
         'background-color': props.backgroundColor ?? defaultBackgroundColor,
